88ac4aa7a4459a79b9cbef37fe47ad4601b96ed4
Godusop46 <sileshinolan@gmail.com>
2026-07-09 19:59:32 +0300

    finished project

--- src/bodyFiles/contact.jsx
+++ src/bodyFiles/contact.jsx
@@ -1,9 +1,38 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import "../static/css/contactStyle.css";
 
 function Contact() {
   useEffect(() => {
     document.title = "Contact me";
   }, []);
-  return (<div className="contact-full-container"></div>);
+  const [textShadowPos , changeTextShadowPos] = useState({
+    x: 10,
+    y: 10,
+  });
+
+  useEffect(() => {
+    const textShadowCalc = (event) => {
+      const xP = (event.clientX - 750) / -20;
+      const yP = (event.clientY - 270) / -25;
+
+      changeTextShadowPos({ x: xP, y: yP });
+    };
+
+    window.addEventListener("mousemove" , textShadowCalc);
+
+    return () => {window.removeEventListener("mousemove", textShadowCalc)}
+  }, []);
+
+  return (
+    <div
+      className="contact-full-container"
+      style={{
+        "--transX": `${textShadowPos.x}px`,
+        "--transY": `${textShadowPos.y}px`,
+      }}
+    >
+      <p className="text">uslkoorginal@gmail.com</p>
+    </div>
+  );
 }
 export default Contact;
